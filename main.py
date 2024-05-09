ce643a5224da70c72d66f33b85838e84463f5601
Vitalik <vitalii.lutsyshyn.liceum2@gmail.com>
2024-05-09 17:30:56 +0300

    efsdf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import random
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication,QMainWindow,QPushButton,QLabel,QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication,QMainWindow,QPushButton,QLabel,QVBoxLayout, QWidget,QFileDialog
 from ui_window import Ui_MainWindow
 from qt_material import apply_stylesheet
 
@@ -13,7 +13,18 @@ class MusicPlayer(QMainWindow):
         self.setGeometry(100, 100, 300, 150)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.connects()
+        self.files = None
 
+    def connects(self):
+        self.ui.addFolder.clicked.connect(self.choose_files)
+
+    def choose_files(self):
+        try:
+                self.files = QFileDialog.getOpenFileNames(self,"Обреіть пісні",filter= "All files(*.mp3 *.wav)")
+        except:
+             print("спробуй ще раз")
+        self.ui.songlist.addItems(self.files)
 
 app = QApplication([])#створення
 window = MusicPlayer()#створення вікна
