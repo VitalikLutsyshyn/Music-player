c10f1d520bd91bb5248ad309d67529302ef24838
Vitalik <vitalii.lutsyshyn.liceum2@gmail.com>
2024-05-14 16:46:21 +0300

    aasdw

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
 import random
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt,QUrl
 from PySide6.QtWidgets import QApplication,QMainWindow,QPushButton,QLabel,QVBoxLayout, QWidget,QFileDialog
+from PySide6.QtMultimedia import *
 from ui_window import Ui_MainWindow
 from qt_material import apply_stylesheet
 
+
+
+ 
 
 class MusicPlayer(QMainWindow):
     def __init__(self):
@@ -13,18 +17,35 @@ class MusicPlayer(QMainWindow):
         self.setGeometry(100, 100, 300, 150)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        #ВІДТВОРЕННЯ МУЗИКИ
+        self.player = QMediaPlayer()
+        self.audioOutput = QAudioOutput()
+        self.player.setAudioOutput(self.audioOutput)
+
         self.connects()
         self.files = None
+        self.current_song = None
 
     def connects(self):
         self.ui.addFolder.clicked.connect(self.choose_files)
+        self.ui.songlist.currentRowChanged.connect(self.open_song)
 
     def choose_files(self):
         try:
                 self.files = QFileDialog.getOpenFileNames(self,"Обреіть пісні",filter= "All files(*.mp3 *.wav)")
         except:
              print("спробуй ще раз")
-        self.ui.songlist.addItems(self.files)
+        self.ui.songlist.addItems(self.files[0])
+
+    def open_song(self):
+         if self.ui.songlist.currentRow() >= 0:#повертає номер рядка
+              self.current_song = self.ui.songlist.currentItem().text()#вибір поточної пісні
+              self.player.setSource(QUrl.fromLocalFile(self.current_song))
+              self.audioOutput.setVolume(50)
+              self.player.play()
+              
+
+
 
 app = QApplication([])#створення
 window = MusicPlayer()#створення вікна
